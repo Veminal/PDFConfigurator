3ffecdbc2b59cf1070cb26b9ed0510d02293ae47
Factura122 <factura122@gmail.com>
2017-05-21 16:09:53 +0600

    JUnit using DI

---
+++ src/test/java/com/veminal/pdf/configuration/read/ReadDataListTest.java
@@ -0,0 +1,61 @@
+package com.veminal.pdf.configuration.read;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.veminal.pdf.core.modules.ConfigurationModule;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Read data list test.
+ *
+ * @author Veminal
+ * @version 1.0
+ */
+public class ReadDataListTest {
+    /**
+     * Read config.
+     */
+    private ReadConfig readConfig;
+
+    /**
+     * Initializing.
+     */
+    @Before
+    public void setUp() {
+        Injector injectMenu = Guice.createInjector(
+                new ConfigurationModule());
+        readConfig = injectMenu.getInstance(ReadDataList.class);
+    }
+
+    /**
+     * Test class.
+     */
+    @Test
+    public void result() {
+        final String path = "images.json";
+        readConfig.readPath(path);
+        List result = (List)readConfig.parse("path");
+        List<String> wait = new ArrayList<>();
+        wait.add("images/new.png");
+        wait.add("images/open.png");
+        wait.add("images/save.png");
+        wait.add("images/del.png");
+        wait.add("images/cut.png");
+        wait.add("images/copy.png");
+        wait.add("images/paste.png");
+        wait.add("images/font.png");
+        wait.add("images/cut_number.png");
+        wait.add("images/fragment_select.png");
+        wait.add("images/split_page.png");
+        wait.add("images/build.png");
+        wait.add("images/inject.png");
+        wait.add("images/settings.png");
+        assertEquals(wait, result);
+    }
+}
