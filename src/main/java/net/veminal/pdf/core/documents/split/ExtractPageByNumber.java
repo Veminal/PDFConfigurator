76c1b4188f45b031d3f1f087b7fc81777b52e0e8
Factura122 <factura122@gmail.com>
2017-08-03 21:47:19 +0600

    Create extract page class.

--- src/main/java/net/veminal/pdf/core/documents/split/ExtractPageByNumber.java
+++ src/main/java/net/veminal/pdf/core/documents/split/ExtractPageByNumber.java
@@ -1,7 +1,8 @@
 package net.veminal.pdf.core.documents.split;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
-import com.itextpdf.layout.Document;
+import com.itextpdf.kernel.pdf.PdfWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,7 +47,8 @@ public final class ExtractPageByNumber implements ISplit {
      * @param t    the String
      * @param name the String
      */
-    public ExtractPageByNumber(final String file, final int n, final String t, final String name) {
+    public ExtractPageByNumber(final String file, final int n,
+                               final String t, final String name) {
         this.filename = file;
         this.number = n;
         this.target = t;
@@ -55,9 +57,16 @@ public final class ExtractPageByNumber implements ISplit {
 
     @Override
     public void extract() {
-        logger.info("extract page");
+        logger.info("EXTRACT PAGE");
         try {
-            PdfReader reader = new PdfReader(filename);
+            PdfDocument mainDoc = new PdfDocument(new PdfReader(filename));
+            PdfDocument document = new PdfDocument(
+                    new PdfWriter(target + fileNameTarget + ".pdf"));
+            mainDoc.copyPagesTo(number, number, document);
+            logger.info("Copy page");
+            mainDoc.close();
+            document.close();
+            logger.info("OPERATION COMPLETED");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
